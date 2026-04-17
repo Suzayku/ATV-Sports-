9cadc7f887681700abd1e7c0989dcece33a3ba8a
Your Name <you@example.com>
2026-04-17 12:19:41 +0700

    Fix: Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+    base: '/ATV-Sports-/',
     plugins: [react()],
     build: {
         outDir: 'docs'
